a5a1de473c1ede41096fe1206c9be1617848a522
kyosuke-takei <rindawan0930@gmail.com>
2026-04-16 11:39:31 +0900

    fix puppeteer headless for github actions

--- scrape.js
+++ scrape.js
@@ -4,7 +4,10 @@ const fs = require('fs');
 puppeteer.use(StealthPlugin());
 
 async function scrapeAllPages(startUrl, label) {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36');
 
